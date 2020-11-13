79315f35e8c51c8284c524eb3b23eb4afea5d194
Rene Lazo <stark@aleph.engineering>
2020-11-12 19:05:05 -0500

    Refactor SharedPreferences Service

--- lib/src/services/shared_preferences_service.dart
+++ lib/src/services/shared_preferences_service.dart
@@ -5,20 +5,22 @@ enum SharePrefsAttribute {
   IS_DARK,
 }
 
+extension ParseToString on SharePrefsAttribute {
+  String toShortString() {
+    return this.toString().split('.').last.toLowerCase();
+  }
+}
+
 class SharedPreferencesService {
   SharedPreferences _prefs;
 
-  String _getAttributeStr(SharePrefsAttribute attribute) {
-    return attribute == SharePrefsAttribute.IS_DARK ? 'is_dark' : '';
-  }
-
   Future loadInstance() async => _prefs = await SharedPreferences.getInstance();
 
-  bool isDark() =>
-      _prefs.getBool(_getAttributeStr(SharePrefsAttribute.IS_DARK));
+  bool isDark() => _prefs.getBool(SharePrefsAttribute.IS_DARK.toShortString());
+
   setIsDark(bool value) =>
-      _prefs.setBool(_getAttributeStr(SharePrefsAttribute.IS_DARK), value);
+      _prefs.setBool(SharePrefsAttribute.IS_DARK.toShortString(), value);
 
   clearPref(SharePrefsAttribute attribute) =>
-      _prefs.remove(_getAttributeStr(attribute));
+      _prefs.remove(attribute.toShortString());
 }
