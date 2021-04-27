8e86cb65bed69e5b8fedbc9981d59e310c354ec1
Rene Lazo <stark@aleph.engineering>
2021-04-27 11:04:28 -0400

    Remove specific code for web

--- lib/easy_dynamic_theme.dart
+++ lib/easy_dynamic_theme.dart
@@ -1,4 +1,3 @@
 export 'package:easy_dynamic_theme/src/easy_dynamic_widget.dart';
 export 'package:easy_dynamic_theme/src/widgets/easy_dynamic_theme_btn.dart';
 export 'package:easy_dynamic_theme/src/widgets/easy_dynamic_theme_switch.dart';
-export 'package:easy_dynamic_theme/easy_dynamic_theme_web.dart';
